dc1ab09d3c58e7c62d89902fc608c110c0674c82
abelardinel2 <ameliabelardinelli@gmail.com>
2025-07-07 13:07:33 -0400

    clean

--- game.js
+++
@@ -1,8 +0,0 @@
-
-const grid = document.getElementById('gardenGrid');
-for (let i = 0; i < 25; i++) {
-  const tile = document.createElement('div');
-  tile.classList.add('tile');
-  tile.addEventListener('click', () => tile.classList.toggle('bloom'));
-  grid.appendChild(tile);
-}
